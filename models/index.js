cf1ca08a76a018e158a2a379f9797170ee24783b
Marketing Director <nicolestark1103@gmail.com>
2023-09-08 10:19:59 -0500

    Added Comment model and added associations to index.js in models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,34 @@
 const User = require('./User');
-// Resume
+const Resume = require('./Resume')
+const Comment = require('./Comment')
 
-module.exports = { User };
+// Associations
+User.hasMany(Resume, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Resume.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Resume.hasMany(Comment, {
+    foreignKey: 'resume_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Resume, {
+    foreignKey: 'resume_id'
+})
+
+module.exports = {
+    User,
+    Resume,
+    Comment
+};
