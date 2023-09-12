c7bc24b38cd51e85adc196c5f35adba7ac018251
Nicole Stark <nicolestark@Nicoles-iMac.attlocal.net>
2023-09-11 20:42:37 -0500

    resume showing up on profile page

--- controllers/api/createRoutes.js
+++ controllers/api/createRoutes.js
@@ -1,34 +1,91 @@
 const router = require('express').Router();
 const { Resume, User } = require('../../models')
 
-// create new resume
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
     try{
-        const newResume = await Resume.findAll({
+        const postedResumes = await Resume.findAll({
             include: [
                 {
-                    model: Resume,
-                    attributes: [
-                        'fname', 
-                        'lname', 
-                        'email', 
-                        'github', 
-                        'company1', 
-                        'role1', 
-                        'description1', 
-                        'rolestart', 
-                        'roleend', 
-                        'institute1', 
-                        'degree1', 
-                        'edstart', 
-                        'edend', 
-                        'skill1', 
-                        'skill2', 
-                        'skill3',
-                    ],
-                },
-            ],
-    })
+                    model: Comment,
+                    include: [{ model: User }]
+                }
+            ]
+        })
+
+        const resumes = postedResumes.map((resume) => resume.get({ plain: true }))
+        res.status(200).json({ resumes })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const resume = await Resume.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Comment,
+                    include: [
+                        { model: User}
+                    ]
+                }
+            ]
+        })
+        const singleResume = resume.get({ plain: true })
+        res.status(200).json({singleResume})
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// create new resume
+router.post('/', async (req, res) => {
+    try {
+        const resume = await Resume.create({
+            fname: req.body.fname,
+            lname: req.body.lname,
+            email: req.body.email,
+            github: req.body.github,
+            company1: req.body.company1,
+            role1: req.body.role1,
+            description1: req.body.description1,
+            rolestart1: req.body.rolestart1,
+            roleend1: req.body.roleend1,
+            institute1: req.body.institute1,
+            degree1: req.body.degree1,
+            edstart1: req.body.edstart1,
+            edend1: req.body.end1,
+            skill1: req.body.skill1,
+            skill2: req.body.skill2,
+            skill3: req.body.skill3
+        })
+        res.status(200).json({ resume, message: `Resume Created` })
+
+    //     const newResume = await Resume.findAll({
+    //         include: [
+    //             {
+    //                 model: Resume,
+    //                 attributes: [
+    //                     'fname', 
+    //                     'lname', 
+    //                     'email', 
+    //                     'github', 
+    //                     'company1', 
+    //                     'role1', 
+    //                     'description1', 
+    //                     'rolestart', 
+    //                     'roleend', 
+    //                     'institute1', 
+    //                     'degree1', 
+    //                     'edstart', 
+    //                     'edend', 
+    //                     'skill1', 
+    //                     'skill2', 
+    //                     'skill3',
+    //                 ],
+    //             },
+    //         ],
+    // })
 
     // save resume
 
