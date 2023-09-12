3079f0a6febdd080079b2745f9f0aef0968af41a
Nicole Stark <nicolestark@Nicoles-iMac.attlocal.net>
2023-09-11 20:45:51 -0500

    added createRoute

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const createRoutes = require('./createRoutes')
 
 router.use('/users', userRoutes);
+router.use('/create', createRoutes)
 
 module.exports = router;
